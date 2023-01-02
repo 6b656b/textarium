c9c4d4b98b796915f8b740485104a5939914da0d
Станислав Добряков <sdobryakov@m2air-std.local>
2023-01-02 16:15:09 +0200

    first working mkdocstrings setup

---
+++ textarium/preprocessing.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+Text cleaning and preprocessing functions.
+"""
+
+import re
+import string
+import unicodedata
+from typing import List, Set, Union
+
+
+def remove_extra_spaces(text: str) -> str:
+    """_summary_
+
+    Args:
+        text (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    text_wo_extra_spaces = " ".join(text.split())
+
+    return text_wo_extra_spaces.strip()
+
+def remove_charset(text: str, charset: Set) -> str:
+    """_summary_
+
+    Args:
+        text (str): _description_
+        charset (Set): _description_
+
+    Returns:
+        str: _description_
+    """
+    text_wo_charset = text.translate(
+        str.maketrans(charset, " " * len(charset))
+    )
+    text_wo_charset = remove_extra_spaces(text_wo_charset)
+
+    return text_wo_charset
+
+
