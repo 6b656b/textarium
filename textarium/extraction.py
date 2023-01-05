322ed561a533c4375ed48933fb36502dca9180ef
6b656b <stdobr@gmail.com>
2023-01-05 12:02:35 +0200

    add extract_sentences function

--- textarium/extraction.py
+++ textarium/extraction.py
@@ -5,6 +5,8 @@ Function for extracting information from texts.
 """
 
 from typing import List
+from razdel import sentenize
+from nltk import tokenize
 
 def extract_tokens(text: str, tokenizer) -> List[str]:
     """Extract tokens from a text
@@ -18,3 +20,20 @@ def extract_tokens(text: str, tokenizer) -> List[str]:
     """
     tokens = tokenizer.tokenize(text)
     return tokens
+
+def extract_sentences(text: str, lang='en') -> List[str]:
+    """Splits text string to a list of sentences
+
+    Args:
+        text (str): Any string in English or Russian
+        lang (str): Text language ('ru' - Russian or 'en' - English)
+
+    Returns:
+        List[str]: A list of extracted sentences
+    """
+    if lang == 'en':
+        sentences = tokenize.sent_tokenize(text)
+    elif lang == 'ru':
+        sentences = [i.text for i in sentenize(text)]
+    return sentences
+
